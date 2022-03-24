d955294851ba4555059b34b44f1c878d3c69cc89
Ihor Myronishyn <myronishun.ihor@icloud.com>
2022-03-24 18:54:28 +0200

    Added a function to customize the TabBar and icons for the main and settings screens.

---
+++ CustomTabBar/CustomTabBarController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  CustomTabBar
+//
+//  Created by myronishyn.ihor on 24.03.2022.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setupTabBar()
+    }
+    
+    private func setupTabBar() {
+        let spacing: CGFloat = 10.0
+        let defaultTabBarHeight: CGFloat = 49.0
+        let desiredTabBarHeight: CGFloat = 65.0
+        
+        additionalSafeAreaInsets = UIEdgeInsets(top: 0, left: 0, bottom: spacing, right: 0)
+        
+        tabBar.frame.origin.x = spacing
+        tabBar.frame.size.width -= spacing * 2
+        tabBar.frame.origin.y -= desiredTabBarHeight - defaultTabBarHeight
+        tabBar.frame.size.height = desiredTabBarHeight
+        
+        tabBar.unselectedItemTintColor = .lightGray
+        tabBar.layer.cornerRadius = 15
+        
+        if let items = tabBar.items {
+            for item in items {
+                item.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -spacing)
+            }
+        }
+    }
+}
+
