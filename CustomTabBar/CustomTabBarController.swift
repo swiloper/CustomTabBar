937815ea58befc4b200565bacc238a848ffc0242
Ihor Myronishyn <myronishun.ihor@icloud.com>
2022-03-25 22:35:29 +0200

    Comments added.

--- CustomTabBar/CustomTabBarController.swift
+++ CustomTabBar/CustomTabBarController.swift
@@ -14,6 +14,7 @@ class CustomTabBarController: UITabBarController {
         setupTabBar()
     }
     
+    /// This function changes the appearance of the tabBar.
     private func setupTabBar() {
         let spacing: CGFloat = 10.0
         let defaultTabBarHeight: CGFloat = 49.0
@@ -21,13 +22,17 @@ class CustomTabBarController: UITabBarController {
         
         additionalSafeAreaInsets = UIEdgeInsets(top: 0, left: 0, bottom: spacing, right: 0)
         
+        // Moves tabBar to the right by spacing
         tabBar.frame.origin.x = spacing
+        // Reduces the size of tabBar, taking into account the indentation on the right and left
         tabBar.frame.size.width -= spacing * 2
+        // Changes the y-coordinate position of the tabBar by the difference between the standard tabBar height and the custom one
         tabBar.frame.origin.y -= desiredTabBarHeight - defaultTabBarHeight
         tabBar.frame.size.height = desiredTabBarHeight
         
         tabBar.unselectedItemTintColor = .lightGray
         tabBar.layer.cornerRadius = 15.0
+        tabBar.layer.masksToBounds = true
         
         if let items = tabBar.items {
             for item in items {
